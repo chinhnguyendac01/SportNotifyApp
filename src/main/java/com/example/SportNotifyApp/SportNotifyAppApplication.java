3ca33df76077a1e0d34728b29ee1e37dbc5d649a
chinhnguyen <chinhnguyendac123@gmail.com>
2025-06-30 22:42:31 +0700

    Initial commit

---
+++ src/main/java/com/example/SportNotifyApp/SportNotifyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SportNotifyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportNotifyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportNotifyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SportNotifyApp/SportNotifyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SportNotifyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportNotifyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
